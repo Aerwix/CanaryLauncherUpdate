8cbbf44ab96f98cacf40ae8ee2dad22f38c4db65
Aerwix <dekarzfajardo@gmail.com>
2023-02-09 13:18:53 -0600

    Update SplashScreen.xaml.cs

--- SplashScreen.xaml.cs
+++ SplashScreen.xaml.cs
@@ -13,7 +13,7 @@ namespace CanaryLauncherUpdate
 		DispatcherTimer timer = new DispatcherTimer();
 		string urlClient = "https://github.com/lucasgiovannibr/clientlauncherupdate/archive/refs/heads/main.zip";
 		string urlVersion = "https://raw.githubusercontent.com/lucasgiovannibr/clientlauncherupdate/main/version.txt";
-		string currentVersion = "";
+		string currentVersion = "0.0.2";
 		string path = AppDomain.CurrentDomain.BaseDirectory.ToString();
 
 		public SplashScreen()
